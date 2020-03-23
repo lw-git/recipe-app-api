ae7b24556a717e05bd8c61af1e18b432dfe26e3a
lw <lw_git@mail.ru>
2020-03-23 16:45:57 +0000

    Added endpoint for creating and authenticating users.

--- app/app/settings.py
+++ app/app/settings.py
@@ -38,8 +38,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # 3rd party
+    'rest_framework',
+    'rest_framework.authtoken',
+
     # local
     'core.apps.CoreConfig',
+    'user.apps.UserConfig',
 ]
 
 MIDDLEWARE = [
